7be79e869502664f3f0baaca0fed30590131bbdd
MairaTariq16 <matariq.bscs18seecs@seecs.edu.pk>
2021-06-05 12:34:44 +0500

    Clear button, shape fill button

--- paint.py
+++ paint.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk, colorchooser
+from tkinter import ttk, colorchooser,font
 import multiprocessing
 import json
 from _thread import start_new_thread
@@ -18,6 +18,7 @@ class main:
         self.drawType = "Pen"
         self.color_fg = 'black'
         self.color_bg = 'white'
+        self.shape_color= 'None'
         self.old_x = None
         self.old_y = None
         self.penwidth = 5
@@ -113,45 +114,62 @@ class main:
     def save_canvas(self):  #changing the background color canvas
         print("Saved")
 
+    def shapeFill(self,btn):
+        if(btn['relief']=='raised'): 
+            #SET shape_fill color
+            self.shape_color = colorchooser.askcolor(title="Shape Fill Color")[1]
+            btn.configure(relief = 'sunken',bg = self.shape_color,fg = self.shape_color)
+            print(self.shape_color)
+        else:
+            self.shape_color='None'
+            btn.configure(relief = 'raised',bg = 'light grey',fg='black',text='Fill')
+
+
     def drawWidgets(self):
 
         self.master.config(bg="skyblue") # specify background color
         # Create top and bottom frames
         top_frame = Frame(self.master, width=500, height=200, bg='white')
-        top_frame.grid(row=0, column=0, padx=10, pady=5,ipady=5,ipadx=5)
+        top_frame.grid(row=0, column=0, padx=10, pady=5)
         bottom_frame = Frame(self.master, width=550, height=600, bg='grey')
         bottom_frame.grid(row=1, column=0, padx=10, pady=5)
             
         # Create frames and labels in left_frame 
-        Label(top_frame, text="Session Code: ").grid(row=0, column=0,pady=5)
-        Label(top_frame, text=self.key).grid(row=0, column=1,pady=5)
+        Label(top_frame, text="Session Code: ").grid(row=0, column=0)
+        t = Text(top_frame,height=1,width=10)
+        t.insert('end', self.key)
+        t.configure(state='disabled')
+        t.grid(row=0, column=1)
+        
 
-        fg_btn =Button(top_frame, text="Pen Color",command=self.change_fg)
-        fg_btn.grid(row=1, column=0)
+        Button(top_frame, text="Pen Color",command=self.change_fg,width=8).grid(row=1, column=0)
 
-        bg_btn =Button(top_frame, text="BG Color",command=self.change_bg)
-        bg_btn.grid(row=1, column=1)
+        Button(top_frame, text="BG Color",command=self.change_bg).grid(row=1, column=1)
 
-        circle_btn =Button(top_frame, text="Circle",command= lambda: self.set_drawType("Circle"))
-        circle_btn.grid(row=1, column=2)
+        Button(top_frame, text="Clear",command=self.clear).grid(row=1, column=2)
 
-        rect_btn =Button(top_frame, text="Rect",command= lambda: self.set_drawType("Rect"))
-        rect_btn.grid(row=1, column=3)
+        Label(top_frame, text='SHAPES:').grid(row=1,column=3,padx= 10)
 
-        pen_btn =Button(top_frame, text="Pen",command=lambda: self.set_drawType("Pen"))
-        pen_btn.grid(row=1, column=4)
+        Button(top_frame, text="Circle",command= lambda: self.set_drawType("Circle")).grid(row=1, column=4)
 
-        line_btn =Button(top_frame, text="Line",command= lambda: self.set_drawType("Line"))
-        line_btn.grid(row=1, column=5)
+        Button(top_frame, text="Rect",command= lambda: self.set_drawType("Rect")).grid(row=1, column=5)
 
-        Label(top_frame, text='Pen Width:').grid(row=1,column=6,padx= 5)
+        Button(top_frame, text="Pen",command=lambda: self.set_drawType("Pen")).grid(row=1, column=6)
+
+        Button(top_frame, text="Line",command= lambda: self.set_drawType("Line")).grid(row=1, column=7)
+
+        myfont = font.Font(overstrike = 1,size=10)
+        shapeFill_btn =Button(top_frame, text="Fill",bg = 'light grey',font=myfont)
+        shapeFill_btn.config(command=lambda : self.shapeFill(shapeFill_btn))
+        shapeFill_btn.grid(row=1, column=8,padx=5)        
+
+        Label(top_frame, text='Pen Width:').grid(row=1,column=9,padx= 5)
         self.slider = ttk.Scale(top_frame,from_= 5, to = 100,command=self.changeW,orient=HORIZONTAL)
         self.slider.set(self.penwidth)
-        self.slider.grid(row=1,column=7,ipadx=10,padx=5)
+        self.slider.grid(row=1,column=10,ipadx=10,padx=5)
 
         
-        save_btn =Button(top_frame, text="Save",command= self.save_canvas)
-        save_btn.grid(row=1, column=8,padx=15)
+        Button(top_frame, text="Save",command= self.save_canvas).grid(row=1, column=12,padx=5)
             
             
         # Display canvas in right_frame
